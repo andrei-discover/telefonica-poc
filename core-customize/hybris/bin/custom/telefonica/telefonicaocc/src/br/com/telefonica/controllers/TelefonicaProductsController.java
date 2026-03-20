984c0939e8d530d7343474171cec0b5de2363702
Gabriel Escobar <gabriel.escobar@discover.com.br>
2026-03-20 10:44:35 -0300

    POC-52 adjustments requested in code review

--- core-customize/hybris/bin/custom/telefonica/telefonicaocc/src/br/com/telefonica/controllers/TelefonicaProductsController.java
+++ core-customize/hybris/bin/custom/telefonica/telefonicaocc/src/br/com/telefonica/controllers/TelefonicaProductsController.java
@@ -13,7 +13,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
-
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
